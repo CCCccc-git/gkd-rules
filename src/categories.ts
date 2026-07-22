af97297b6a87b2d193c68a2f9637de34c77aa67c
CCCccc-git <chentongxue0305@gmail.com>
2026-07-22 20:50:02 +0800

    Add new category for 开屏广告

--- src/categories.ts
+++ src/categories.ts
@@ -1,3 +1,9 @@
 import { defineGkdCategories } from '@gkd-kit/define';
 
-export default defineGkdCategories([]);
+export default defineGkdCategories([
+  {
+    key: 0,
+    name: '开屏广告',
+    enable: true,
+  },
+]);
