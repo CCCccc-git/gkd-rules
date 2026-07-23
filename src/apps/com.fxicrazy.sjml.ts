8f5826554025196268c575dbc80acf1a7243c980
CCCccc-git <chentongxue0305@gmail.com>
2026-07-23 10:07:31 +0800

    Add new GKD app definition for 联掌门户

---
+++ src/apps/com.fxicrazy.sjml.ts
@@ -0,0 +1,29 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.fxicrazy.sjml',
+  name: '联掌门户',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: ['.ui.welcome.WelcomeActivity'],
+      fastQuery: true,
+      matchRoot: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          key: 0,
+          name: '点击右上角跳过',
+          matches: '[id="com.fxicrazy.sjml:id/ptgAdvertLayout"][visibleToUser=true]',
+          position: {
+            x: 'screenWidth*0.9',
+            y: 'screenHeight*0.12',
+          },
+        },
+      ],
+    },
+  ],
+});
